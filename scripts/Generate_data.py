a8c1ebe338f88998a982919e943db8c0f4c0b80f
Manish Sarkar <sarkarmrigank28@gmail.com>
2025-07-05 13:29:48 +0530

    Generate Data ++ && -- requirements.txt

--- scripts/Generate_data.py
+++ scripts/Generate_data.py
@@ -13,6 +13,14 @@ def generate_fake_UPI_transactions(n = 1000, save_directory = "../UPI-vanguard/d
     data = []
 
     for i in range(n):
+        # Generate a fake transaction
+        # transaction_id is a string of 6 digits, timestamp is a random date in the past 100000 days,
+        # sender_id and receiver_id are random integers between 1000 and 9999,
+        # amount is a random float between 1.0 and 1000.0,
+        # device is a random choice from the devices list, city is a random choice from the cities list,
+        # transaction_type is a random choice from the transactions_types list,
+        # status is a random choice from the status list
+
         trn = {
             "transaction_id": f"T{i:06d}",
             "timestamp": (datetime.now() - timedelta(days=random.randint(0, 100000))).strftime("%Y-%m-%d %H:%M:%S"),
@@ -24,6 +32,7 @@ def generate_fake_UPI_transactions(n = 1000, save_directory = "../UPI-vanguard/d
             "transaction_type": random.choice(transactions_types),
             "status": random.choice(status)
         }
+        
         # Now to avoid getting transactions as sender = receiver
         while trn["sender_id"] == trn["receiver_id"]:
             trn["receiver_id"] = random.randint(1000, 9999)
@@ -40,4 +49,8 @@ def generate_fake_UPI_transactions(n = 1000, save_directory = "../UPI-vanguard/d
 
 if __name__ == "__main__":
     #Generate 1000 fake UPI transactions
+    #if not given the number of transactions, it will default to 1000
+    #if not given the save directory, it will default to "../UPI-vanguard/data/upi_transactions.csv"
+    #so you can call the function with just the number of transactions
+    #or with both the number of transactions and the save directory
     generate_fake_UPI_transactions(n = 1000)
